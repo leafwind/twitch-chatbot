083ce2e108306cfa9177f2c51ae6eb8265d4fd4c
connor <leafwind.cs@gmail.com>
2021-03-31 21:45:20 +0900

    chore: fix lint

--- scripts/manual_serialization.py
+++ scripts/manual_serialization.py
@@ -1,10 +1,19 @@
+# manually write the binary data
 import dill
 from expiringdict import ExpiringDict
+
 with open("data/harnaisxsumire666.bin", "wb") as f:
-    data = {"gbf_room_num": 139, "gbf_room_id_cache": ExpiringDict(max_len=1, max_age_seconds=600)}
+    data = {
+        "gbf_room_num": 139,
+        "gbf_room_id_cache": ExpiringDict(max_len=1, max_age_seconds=600),
+    }
     data["gbf_room_id_cache"]["user_id"] = "DA3F9600"
     f.write(dill.dumps(data))
 
+
+# manually check the binary data
+
 import dill
+
 with open("data/harnaisxsumire666.bin", "rb") as f:
     print(dill.loads(f.read()))
