969aba59fd685ae98926769ba958f8277c280ccf
connor <leafwind.cs@gmail.com>
2022-06-07 23:44:53 +0900

    refactor: import

--- chatbot.py
+++ chatbot.py
@@ -7,8 +7,6 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 """
-import dill
-import irc.bot
 import logging
 import math
 import multiprocessing
@@ -18,7 +16,9 @@ import re
 import signal
 import sys
 import time
-import time
+
+import dill
+import irc.bot
 import yaml
 from expiringdict import ExpiringDict
 
